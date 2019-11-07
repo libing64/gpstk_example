75e7aa7b94dbe7c652567f332b9ecfb24ed6fa99
libing64 <libing64@gmail.com>
2019-11-07 08:57:19 +0800

    format source code

--- example.cpp
+++ example.cpp
@@ -15,7 +15,7 @@
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with GPSTk; if not, write to the Free Software Foundation,
 //  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
-//  
+//
 //  Copyright 2004-2019, The University of Texas at Austin
 //
 //============================================================================
@@ -23,13 +23,13 @@
 //============================================================================
 //
 //This software developed by Applied Research Laboratories at the University of
-//Texas at Austin, under contract to an agency or agencies within the U.S. 
+//Texas at Austin, under contract to an agency or agencies within the U.S.
 //Department of Defense. The U.S. Government retains all rights to use,
-//duplicate, distribute, disclose, or release this software. 
+//duplicate, distribute, disclose, or release this software.
 //
-//Pursuant to DoD Directive 523024 
+//Pursuant to DoD Directive 523024
 //
-// DISTRIBUTION STATEMENT A: This software has been approved for public 
+// DISTRIBUTION STATEMENT A: This software has been approved for public
 //                           release, distribution is unlimited.
 //
 //=============================================================================
@@ -51,125 +51,119 @@ using namespace gpstk;
 
 void printMonth(short month, short year)
 {
-   CivilTime civ(year, month, 1, 0, 0, 0.0);
-  
-   cout << endl << civ.printf("%26b %4Y") << endl;
-   
-   GPSWeekSecond gws(civ);
+    CivilTime civ(year, month, 1, 0, 0, 0.0);
 
-   for (; civ.month == month; ++gws.week, gws.sow = 0, civ = gws)
-   {
-      cout << setw(4) << gws.week << "  ";
-      
-      for (short thisDow = 0; thisDow < 7; ++thisDow)
-      {
-         gws.sow = thisDow * SEC_PER_DAY;
-         CommonTime com(gws);
-         if (CivilTime(com).month == month)
-	   cout << printTime(com, "%2d-%03j ");
-         else 
-           cout << "       ";
-      }
-      cout << endl;
-   }
+    cout << endl
+         << civ.printf("%26b %4Y") << endl;
+
+    GPSWeekSecond gws(civ);
+
+    for (; civ.month == month; ++gws.week, gws.sow = 0, civ = gws)
+    {
+        cout << setw(4) << gws.week << "  ";
+
+        for (short thisDow = 0; thisDow < 7; ++thisDow)
+        {
+            gws.sow = thisDow * SEC_PER_DAY;
+            CommonTime com(gws);
+            if (CivilTime(com).month == month)
+                cout << printTime(com, "%2d-%03j ");
+            else
+                cout << "       ";
+        }
+        cout << endl;
+    }
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 
-   try {
-      
-      CommandOptionNoArg helpOption('h',"help","Display argument list.",false);
-      CommandOptionNoArg threeOption('3',"three-months","Display last, this and next months.",false);
-      CommandOptionNoArg thisYearOption('y',"year","Display all months for the current year");
-      CommandOptionWithNumberArg givenYearOption('Y',"specific-year","Display all months for a given year");
-      CommandOptionNoArg blurbOption('n',"no-blurb","Suppress GPSTk reference in graphic output.");
-      
-      CommandOptionParser cop("GPSTk GPS Calendar Generator");
-      cop.parseOptions(argc, argv);
+    try
+    {
 
-      if (cop.hasErrors())
-      {
-         cop.dumpErrors(cout);
-         cop.displayUsage(cout);
-         return 1;
-      }
+        CommandOptionNoArg helpOption('h', "help", "Display argument list.", false);
+        CommandOptionNoArg threeOption('3', "three-months", "Display last, this and next months.", false);
+        CommandOptionNoArg thisYearOption('y', "year", "Display all months for the current year");
+        CommandOptionWithNumberArg givenYearOption('Y', "specific-year", "Display all months for a given year");
+        CommandOptionNoArg blurbOption('n', "no-blurb", "Suppress GPSTk reference in graphic output.");
 
-      if(helpOption.getCount())
-      {
-         cop.displayUsage(cout);
-         return 0;
-      }
-    
-      
-      // Print this month
-      SystemTime st;
-      CivilTime now(st);
-      int firstMonth = now.month;
-      int lastMonth  = now.month;
-      int firstYear  = now.year;
-      int lastYear   = now.year;
+        CommandOptionParser cop("GPSTk GPS Calendar Generator");
+        cop.parseOptions(argc, argv);
 
-      if (thisYearOption.getCount())
-      {
-         firstMonth =1;
-         lastMonth  =12;
-      }
-      
-      if (givenYearOption.getCount())
-      {
-         firstMonth =1;
-         lastMonth  =12;
-         
-         firstYear = gpstk::StringUtils::asInt((givenYearOption.getValue())[0]);
-         lastYear = firstYear;
-         
-      }
+        if (cop.hasErrors())
+        {
+            cop.dumpErrors(cout);
+            cop.displayUsage(cout);
+            return 1;
+        }
 
-      if (threeOption.getCount())
-      {
-         firstMonth--;
-         if (firstMonth==0)
-         {
-            firstMonth = 12;
-            firstYear--;
-         }
-         
-         lastMonth++;
-         if (lastMonth==13)
-         {
-            lastMonth = 1;
-            lastYear++;
-         }
-      }
-      
-      int mcount=0;
-      for (short m=firstMonth, y=firstYear;
-          (y<lastYear) || ((m<=lastMonth) && (y==lastYear)); 
-           m++)
-      {
-         if (m==13)
-         {
-            m=1;
-            y++;
-         }
-         
-          printMonth(m, y);
+        if (helpOption.getCount())
+        {
+            cop.displayUsage(cout);
+            return 0;
+        }
 
+        // Print this month
+        SystemTime st;
+        CivilTime now(st);
+        int firstMonth = now.month;
+        int lastMonth = now.month;
+        int firstYear = now.year;
+        int lastYear = now.year;
 
-         
-      }
-  
-      cout << endl;
-      
-      
-   }
-   catch( Exception error)
-   {
-      cout << error << endl;
-      exit(-1);
-   }
+        if (thisYearOption.getCount())
+        {
+            firstMonth = 1;
+            lastMonth = 12;
+        }
 
-   exit(0);
+        if (givenYearOption.getCount())
+        {
+            firstMonth = 1;
+            lastMonth = 12;
+
+            firstYear = gpstk::StringUtils::asInt((givenYearOption.getValue())[0]);
+            lastYear = firstYear;
+        }
+
+        if (threeOption.getCount())
+        {
+            firstMonth--;
+            if (firstMonth == 0)
+            {
+                firstMonth = 12;
+                firstYear--;
+            }
+
+            lastMonth++;
+            if (lastMonth == 13)
+            {
+                lastMonth = 1;
+                lastYear++;
+            }
+        }
+
+        int mcount = 0;
+        for (short m = firstMonth, y = firstYear;
+             (y < lastYear) || ((m <= lastMonth) && (y == lastYear));
+             m++)
+        {
+            if (m == 13)
+            {
+                m = 1;
+                y++;
+            }
+
+            printMonth(m, y);
+        }
+
+        cout << endl;
+    }
+    catch (Exception error)
+    {
+        cout << error << endl;
+        exit(-1);
+    }
+
+    exit(0);
 }
-
