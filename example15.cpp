3ee618888bc959da74bb157b750ed13f3e2ce265
libing64 <libing64@gmail.com>
2019-11-09 00:43:21 +0800

    reinstall gpstk and compile all examples

--- example15.cpp
+++ example15.cpp
@@ -1,4 +1,4 @@
-//==============================================================================
+//============================================================================
 //
 //  This file is part of GPSTk, the GPS Toolkit.
 //
@@ -15,24 +15,24 @@
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with GPSTk; if not, write to the Free Software Foundation,
 //  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
+//  
+//  Copyright 2004, The University of Texas at Austin
 //
-//  Copyright 2004-2019, The University of Texas at Austin
-//
-//==============================================================================
+//============================================================================
 
-//==============================================================================
+//============================================================================
 //
-//  This software developed by Applied Research Laboratories at the University of
-//  Texas at Austin, under contract to an agency or agencies within the U.S.
-//  Department of Defense. The U.S. Government retains all rights to use,
-//  duplicate, distribute, disclose, or release this software.
+//This software developed by Applied Research Laboratories at the University of
+//Texas at Austin, under contract to an agency or agencies within the U.S. 
+//Department of Defense. The U.S. Government retains all rights to use,
+//duplicate, distribute, disclose, or release this software. 
 //
-//  Pursuant to DoD Directive 523024
+//Pursuant to DoD Directive 523024 
 //
-//  DISTRIBUTION STATEMENT A: This software has been approved for public
-//                            release, distribution is unlimited.
+// DISTRIBUTION STATEMENT A: This software has been approved for public 
+//                           release, distribution is unlimited.
 //
-//==============================================================================
+//=============================================================================
 
 // An example of robust statistics found in lib/geomatics
 // compute Robust statistics. Also demonstrate the use
@@ -50,100 +50,100 @@ using namespace std;
 using namespace gpstk;
 using namespace Robust;
 
-int main(int argc, char *argv[])
+
+int main(int argc, char* argv[])
 {
-    double *s = 0; // NULL isn't part of the C++ std.
-    double mean = 10.0;
-    double std_dev = 2.0;
-    double bad_measurement = 10000;
+   double *s = 0; // NULL isn't part of the C++ std.
+   double mean = 10.0;
+   double std_dev = 2.0;
+   double bad_measurement = 10000;
 
-    try
-    {
+   try {
 
-        // Generate a set of random numbers that are normally distributed
-        size_t N = 1000;
-        s = new double[N];
+      // Generate a set of random numbers that are normally distributed
+      size_t N = 1000;
+      s= new double[N];
 
-        Stats<double> simpleStats;
+      Stats<double> simpleStats;
 
-        for (size_t i = 0; i < N; i++)
-        {
-            s[i] = RandNorm(std_dev) + mean;
-            // Note the 1.0 keeps the math floating point,
-            // otherwise the division is integer division.
+      for (size_t i = 0; i<N; i++)
+      {
+         s[i] = RandNorm(std_dev)+mean;
+	 // Note the 1.0 keeps the math floating point, 
+         // otherwise the division is integer division.
 
-            simpleStats.Add(s[i]);
-        }
+         simpleStats.Add(s[i]);
+      }
 
-        // Show the sample mean and std deviation before adding perturbed samples.
-        cout << endl
-             << "Before perturbation: sample mean is               "
-             << simpleStats.Average() << ", " << endl;
-        cout << "                     sample standard deviation is "
-             << simpleStats.StdDev() << endl
-             << endl;
+      // Show the sample mean and std deviation before adding perturbed samples.
+      cout  << endl << "Before perturbation: sample mean is               " 
+	    << simpleStats.Average() << ", " << endl;
+      cout  << "                     sample standard deviation is " 
+	    << simpleStats.StdDev() << endl << endl;
 
-        // Now perturb the sample data set with a "bad" measurement.
-        srand(time(0));
-        size_t i = (rand() % N);
-        s[i] = bad_measurement;
-        cout << "Altering measurement " << i << " to take the value of "
-             << bad_measurement << endl;
+      // Now perturb the sample data set with a "bad" measurement.
+      srand(time(0));
+      size_t i = (rand()%N);
+      s[i] = bad_measurement;
+      cout << "Altering measurement " << i << " to take the value of " 
+	   << bad_measurement << endl;
 
-        // Show how the sample mean and std deviation are altered.
-        Stats<double> secondStats;
-        for (size_t i = 0; i < N; i++)
-        {
-            secondStats.Add(s[i]);
-        }
+      // Show how the sample mean and std deviation are altered.
+      Stats<double> secondStats;
+      for (size_t i = 0; i<N; i++)
+      {
+         secondStats.Add(s[i]);
+      }
+					       
+      // Show the sample mean and std deviation before adding perturbed samples.
+      cout  << endl << "After perturbation:  sample mean is                " 
+	    << secondStats.Average() << ", " << endl;
+      cout  << "                     sample standard deviation is " 
+	    << secondStats.StdDev() << endl << endl;
 
-        // Show the sample mean and std deviation before adding perturbed samples.
-        cout << endl
-             << "After perturbation:  sample mean is                "
-             << secondStats.Average() << ", " << endl;
-        cout << "                     sample standard deviation is "
-             << secondStats.StdDev() << endl
-             << endl;
+      double median,mad,Q1,Q3;
 
-        double median, mad, Q1, Q3;
+      QSort(s,N);
+      Robust::Quartiles(s,N,Q1,Q3);
+      mad = Robust::MedianAbsoluteDeviation(s,N,median);
 
-        QSort(s, N);
-        Robust::Quartiles(s, N, Q1, Q3);
-        mad = Robust::MedianAbsoluteDeviation(s, N, median);
+      cout << "Robust statistics:\n";
+      cout << "                     number    = " << N << endl;
+      cout << "                     quartiles = " << setw(11) << setprecision(8) << Q1
+	   << " " << setw(11) << setprecision(8) << Q3 << endl;
+      cout << "                     median    = " << setw(11) << setprecision(8) << median << endl;
+      cout << "                     MAD       = " << setw(11) << setprecision(8) << mad << endl;
 
-        cout << "Robust statistics:\n";
-        cout << "                     number    = " << N << endl;
-        cout << "                     quartiles = " << setw(11) << setprecision(8) << Q1
-             << " " << setw(11) << setprecision(8) << Q3 << endl;
-        cout << "                     median    = " << setw(11) << setprecision(8) << median << endl;
-        cout << "                     MAD       = " << setw(11) << setprecision(8) << mad << endl;
 
-        // Show how the sample mean and std deviation are altered.
-        Stats<double> thirdStats;
-        for (size_t i = 0; i < N; i++)
-        {
-            if ((fabs(s[i] - median) / mad) < 8)
-                thirdStats.Add(s[i]);
-        }
+      // Show how the sample mean and std deviation are altered.
+      Stats<double> thirdStats;
+      for (size_t i = 0; i<N; i++)
+      {
+	if ((fabs(s[i] - median)/mad)<8)
+         thirdStats.Add(s[i]);
+      }
+					       
 
-        // Compute the mean and std deviation now given robust statistics.
-        cout << endl
-             << "Using robust stats:  sample mean is                "
-             << thirdStats.Average() << ", " << endl;
-        cout << "                     sample standard deviation is "
-             << thirdStats.StdDev() << endl
-             << endl;
-    }
-    catch (bad_alloc &b)
-    {
-        cerr << "Allocation error. Out of memory?" << endl;
-        exit(0);
-    }
+      // Compute the mean and std deviation now given robust statistics.
+      cout  << endl << "Using robust stats:  sample mean is                " 
+	    << thirdStats.Average() << ", " << endl;
+      cout  << "                     sample standard deviation is " 
+	    << thirdStats.StdDev() << endl << endl;
 
-    // Delete the dynamic array allocation.
-    // (don't just rely on the operating system to do this).
-    delete[] s;
-    s = 0;
 
-    exit(0);
+
+      
+   }
+   catch (bad_alloc& b)
+   {
+      cerr << "Allocation error. Out of memory?" << endl;
+      exit(0);
+   } 
+
+   // Delete the dynamic array allocation. 
+   // (don't just rely on the operating system to do this).
+   delete[] s;
+   s = 0;
+
+  exit(0);
 }
--- example2.cpp
+++ example2.cpp
@@ -1,4 +1,4 @@
-//==============================================================================
+//============================================================================
 //
 //  This file is part of GPSTk, the GPS Toolkit.
 //
@@ -15,24 +15,24 @@
 //  You should have received a copy of the GNU Lesser General Public
 //  License along with GPSTk; if not, write to the Free Software Foundation,
 //  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
+//  
+//  Copyright 2004, The University of Texas at Austin
 //
-//  Copyright 2004-2019, The University of Texas at Austin
-//
-//==============================================================================
+//============================================================================
 
-//==============================================================================
+//============================================================================
 //
-//  This software developed by Applied Research Laboratories at the University of
-//  Texas at Austin, under contract to an agency or agencies within the U.S.
-//  Department of Defense. The U.S. Government retains all rights to use,
-//  duplicate, distribute, disclose, or release this software.
+//This software developed by Applied Research Laboratories at the University of
+//Texas at Austin, under contract to an agency or agencies within the U.S. 
+//Department of Defense. The U.S. Government retains all rights to use,
+//duplicate, distribute, disclose, or release this software. 
 //
-//  Pursuant to DoD Directive 523024
+//Pursuant to DoD Directive 523024 
 //
-//  DISTRIBUTION STATEMENT A: This software has been approved for public
-//                            release, distribution is unlimited.
+// DISTRIBUTION STATEMENT A: This software has been approved for public 
+//                           release, distribution is unlimited.
 //
-//==============================================================================
+//=============================================================================
 
 #include <iostream>
 #include <iomanip>
@@ -48,98 +48,25 @@ using namespace gpstk;
 int main(void)
 {
 
-    // Create the input file stream
-    Rinex3ObsStream rin("bahr1620.04o");
+      // Create the input file stream
+   Rinex3ObsStream rin("bahr1620.04o");
 
-    // Create the output file stream
-    Rinex3ObsStream rout("bahr1620.04o.new", ios::out | ios::trunc);
+      // Create the output file stream
+   Rinex3ObsStream rout("bahr1620.04o.new", ios::out|ios::trunc);
 
-    // Read the RINEX header
-    Rinex3ObsHeader head; //RINEX header object
-    rin >> head;
-    rout.header = rin.header;
+      // Read the RINEX header
+   Rinex3ObsHeader head;    //RINEX header object
+   rin >> head;
+   rout.header = rin.header;
+   rout << rout.header;
 
-    //add some print
-    cout << endl
-         << head.hsRunBy << "  :PGM / RUN BY / DATE";
-    cout << endl
-         << head.hsVersion << "  :RINEX VERSION / TYPE";
-    cout << endl
-         << head.hsComment << "   :COMMENT";
-    cout << endl
-         << head.hsMarkerName << "  :MARKER NAME";
-    cout << endl
-         << head.hsMarkerNumber << "  :MARKER NUMBER";
-    cout << endl
-         << head.hsMarkerType << "   :MARKER TYPE";
-    cout << endl
-         << head.hsObserver << "       :OBSERVER / AGENCY";
-    cout << endl
-         << head.hsReceiver << "       :REC # / TYPE / VERS";
-    cout << endl
-         << head.hsAntennaType << "     :ANT # / TYPE";
-    cout << endl
-         << head.hsAntennaPosition << "     :APPROX POSITION XYZ";
-    cout << endl
-         << head.hsAntennaDeltaHEN << "     :ANTENNA: DELTA H/E/N";
-    cout << endl
-         << head.hsAntennaDeltaXYZ << "     :ANTENNA: DELTA X/Y/Z";
-    cout << endl
-         << head.hsAntennaPhaseCtr << "     :ANTENNA: PHASECENTER";
-    cout << endl
-         << head.hsAntennaBsightXYZ << "     :ANTENNA: B.SIGHT XYZ";
-    cout << endl
-         << head.hsAntennaZeroDirAzi << "     :ANTENNA: ZERODIR AZI";
-    cout << endl
-         << head.hsAntennaZeroDirXYZ << "     :ANTENNA: ZERODIR XYZ";
-    cout << endl
-         << head.hsCenterOfMass << "     :CENTER OF MASS: XYZ";
-    cout << endl
-         << head.hsNumObs << "     :# / TYPES OF OBSERV";
-    cout << endl
-         << head.hsSystemNumObs << "     :SYS / # / OBS TYPES";
-    cout << endl
-         << head.hsWaveFact << "     :WAVELENGTH FACT L1/2";
-    cout << endl
-         << head.hsSigStrengthUnit << "     :SIGNAL STRENGTH UNIT";
-    cout << endl
-         << head.hsInterval << "     :INTERVAL";
-    cout << endl
-         << head.hsFirstTime << "     :TIME OF FIRST OBS";
-    cout << endl
-         << head.hsLastTime << "     :TIME OF LAST OBS";
-    cout << endl
-         << head.hsReceiverOffset << "     :RCV CLOCK OFFS APPL";
-    cout << endl
-         << head.hsSystemDCBSapplied << "     :SYS / DCBS APPLIED";
-    cout << endl
-         << head.hsSystemPCVSapplied << "     :SYS / PCVS APPLIED";
-    cout << endl
-         << head.hsSystemScaleFac << "     :SYS / SCALE FACTOR";
-    cout << endl
-         << head.hsSystemPhaseShift << "     :SYS / PHASE SHIFT";
-    cout << endl
-         << head.hsGlonassSlotFreqNo << "     :GLONASS SLOT / FRQ #";
-    cout << endl
-         << head.hsGlonassCodPhsBias << "     :GLONASS COD/PHS/BIS";
-    cout << endl
-         << head.hsLeapSeconds << "     :LEAP SECONDS";
-    cout << endl
-         << head.hsNumSats << "     :# OF SATELLITES";
-    cout << endl
-         << head.hsPrnObs << "     :PRN / # OF OBS";
-    cout << endl
-         << head.hsEoH << "     :END OF HEADER";
+      // Loop over all data epochs
+   Rinex3ObsData data;   //RINEX data object
+   while (rin >> data)
+   {
+     rout << data;
+   }
 
-    rout << rout.header;
+   exit(0);
 
-    // Loop over all data epochs
-    Rinex3ObsData data; //RINEX data object
-    while (rin >> data)
-    {
-        cout << "time:  " <<  data.time << endl;
-        rout << data;
-    }
-
-    exit(0);
 }
