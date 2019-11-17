830eed3ee7a33616a85695b92b925f18e015361b
libing64 <libing64@gmail.com>
2019-11-17 16:02:17 +0800

    contruct rtk equation

--- rtk/rinex2rtk.cpp
+++ rtk/rinex2rtk.cpp
@@ -30,8 +30,44 @@
 // Class defining GPS system constants
 #include "GNSSconstants.hpp"
 
+#include <Eigen/Eigen>
+
 using namespace std;
 using namespace gpstk;
+using namespace Eigen;
+
+
+typedef struct 
+{
+    double P1;
+    double P2;
+    double C1;
+    double C2;
+    Xvt sat_xvt;
+    SatID prn;
+} rtk_obs_t;
+
+
+void rtk_solver(vector<rtk_obs_t>& rtk_obs)
+{
+    int n = rtk_obs.size();
+    MatrixXd H = MatrixXd(2*n - 2, n - 1 + 3);
+    VectorXd y = VectorXd(2 * n - 2);
+    for (int i = 0; i < (n - 1); i++)
+    {
+        rtk_obs_t obs1 = rtk_obs[i];
+        rtk_obs_t obs2 = rtk_obs[i + 1];
+        double dd_c = (obs1.C1 - obs1.C2) - (obs2.C1 - obs2.C2);//double difference
+        double dd_p = (obs1.P1 - obs1.P2) - (obs2.P1 - obs2.P2); //double difference
+
+        y(2 * i) = dd_c;
+        y(2 * i + 1) = dd_p;
+
+        //H(2 * i, ) =
+        //H(2 * i + 1, ) = 
+    }
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -112,6 +148,7 @@ int main(int argc, char *argv[])
             cout << "cnt: " << cnt << endl;
             vector<SatID> prnVec;
             vector<double> rangeVec;
+            vector<rtk_obs_t> rtk_obs_q;
 
             // Define the "it" iterator to visit the observations PRN map.
             // Rinex3ObsData::DataMap is a map from RinexSatID to
@@ -125,30 +162,44 @@ int main(int argc, char *argv[])
             // Rinex3ObsData to get the map of observations
             double P1(0.0), C1(0.0);
             double P1_station(0.0), C1_station(0.0);
-            RinexSatID satId;
+            RinexSatID prn;
 
+            rtk_obs_t rtk_obs;
             for (it = rod.obs.begin(); it != rod.obs.end(); it++)
             {
                 // The RINEX file may have P1 observations, but the current
                 // satellite may not have them.
                 try
                 {
+                    prn = it->first;
                     P1 = rod.getObs(it->first, indexP1).data;
                     C1 = rod.getObs(it->first, indexC1).data;
-                    satId = it->first;
-
-
                     P1_station = rod_station.getObs(it->first, indexP1_station).data;
                     C1_station = rod_station.getObs(it->first, indexC1_station).data;
+
+                    //compute sat pos
+                    Xvt sat_xvt = bcestore.getXvt(prn, rod.time);
+                    cout << "satellite pos:" << sat_xvt.x << endl;
+                    cout << "satellite vel:" << sat_xvt.v << endl;
+                    cout << "satellite clock bias: " << sat_xvt.clkbias << endl;
+                    cout << "satellite clock drift: " << sat_xvt.clkdrift << endl;
+
+
+                    rtk_obs.P1 = P1;
+                    rtk_obs.P2 = P1_station;
+                    rtk_obs.C1 = C1;
+                    rtk_obs.C2 = C1_station;
+                    rtk_obs.sat_xvt = sat_xvt;
+                    rtk_obs.prn = prn;
+                    rtk_obs_q.push_back(rtk_obs);
                 }
                 catch (...)
                 {
                     continue;
                 }
 
-
-                cout << "satId:  " << satId << "   P1: " << P1 << "   C1:" << C1 << endl;
-                cout << "station:" << satId << "   P1: " << P1_station << "   C1:" << C1_station << endl;
+                cout << "satId:  " << prn << "   P1: " << P1 << "   C1:" << C1 << endl;
+                cout << "station:" << prn << "   P1: " << P1_station << "   C1:" << C1_station << endl;
                 double ionocorr(0.0);
 
                 // Now, we include the current PRN number in the first part
