a0ce94ab5a6ab942ca8591f9c5e539a9b4b01f22
libing64 <libing64@gmail.com>
2019-12-18 22:32:30 +0800

    simulate with generated data

--- rtk/pvt.cpp
+++ rtk/pvt.cpp
@@ -13,7 +13,7 @@ void genetate_data(vector<pvt_obs_t>& pvt_obs)
 
     for (int i = 0; i < pvt_obs.size(); i++)
     {
-        pvt_obs[i].sat_pos = VectorXd::Random(3);
+        pvt_obs[i].sat_pos = VectorXd::Random(3) * 1000;
         pvt_obs[i].P = distance(pvt_obs[i].sat_pos, x.segment(0, 3)) + x(3);
         cout << "sat: " << i << "  P:" << pvt_obs[i].P << "  " << pvt_obs[i].sat_pos.transpose() << endl;
     }
@@ -30,8 +30,8 @@ void residual(VectorXd x, vector<pvt_obs_t> pvt_obs, VectorXd& res, MatrixXd& H)
         pvt_obs_t obs = pvt_obs[i];
         Vector3d sat_pos = obs.sat_pos;
         double P = obs.P;
-        double rho = distance(sat_pos, x.segment(0, 3)) + x(3);
-        res(i) = P - rho;
+        double rho = distance(sat_pos, x.segment(0, 3));
+        res(i) = P - rho - x(3);
         cout << "P: " << P << "  rho : " << rho << "  y: " << res(i) << endl;
         H(i, 0) = (x(0) - sat_pos(0)) / rho;
         H(i, 1) = (x(1) - sat_pos(1)) / rho;
@@ -43,12 +43,13 @@ void residual(VectorXd x, vector<pvt_obs_t> pvt_obs, VectorXd& res, MatrixXd& H)
 void pvt_solver(vector<pvt_obs_t> pvt_obs)
 {
 
-    //genetate_data(pvt_obs);
+    genetate_data(pvt_obs);
+    VectorXd x = VectorXd::Zero(4); //x, y, z, tau
     //cout << "xx2: " << x.transpose() << endl;
     int n = pvt_obs.size();
 
     //VectorXd x = Vector4d(-3978242.4348, 3382841.1715, 3649902.7667, 0);
-    VectorXd x = VectorXd::Zero(4); //x, y, z, tau
+    
     double err = 1e8;
     int cnt = 0;
     VectorXd y = VectorXd::Zero(n);
@@ -57,7 +58,15 @@ void pvt_solver(vector<pvt_obs_t> pvt_obs)
     {
         cout << "sat: " << i << "  P:" << pvt_obs[i].P  << "  " << pvt_obs[i].sat_pos.transpose() << endl;
     }
-    double mu = 1.0;
+    for (int i = 0; i < n; i++)
+    {
+        pvt_obs_t obs = pvt_obs[i];
+        Vector3d sat_pos = obs.sat_pos;
+        double P = obs.P;
+        double rho = distance(sat_pos, x.segment(0, 3));
+        y(i) = P - rho;
+        cout << "P: " << P << "  rho : " << rho << "  y: " << y(i) << endl;
+    }
     while (err > 0.1 && cnt < 20)
     {
         cout << "cnt : <<<<<<<<<<<<<<<<<<<<<<<<<<" << cnt << endl;
@@ -66,8 +75,8 @@ void pvt_solver(vector<pvt_obs_t> pvt_obs)
             pvt_obs_t obs = pvt_obs[i];
             Vector3d sat_pos = obs.sat_pos;
             double P = obs.P;
-            double rho = distance(sat_pos, x.segment(0, 3)) + x(3);
-            y(i) = P - rho;
+            double rho = distance(sat_pos, x.segment(0, 3));
+            y(i) = P - rho - x(3);
             cout << "P: " << P << "  rho : " << rho  << "  y: " << y(i) << endl;
             H(i, 0) = (x(0) - sat_pos(0)) / rho;
             H(i, 1) = (x(1) - sat_pos(1)) / rho;
