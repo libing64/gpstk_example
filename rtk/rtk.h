236cd6606e3962aa8e62c9857b47692d124838c9
libing64 <libing64@gmail.com>
2019-12-28 23:18:17 +0800

    split rtk.cpp

---
+++ rtk/rtk.h
@@ -0,0 +1,26 @@
+#ifndef __RTK_H
+#define __RTK_H
+
+#include <iostream>
+#include <Eigen/Eigen>
+#include "GPSEphemerisStore.hpp"
+
+using namespace std;
+using namespace Eigen;
+using namespace gpstk;
+
+typedef struct
+{
+    double P1;
+    double P2;
+    double C1;
+    double C2;
+    float pos[3];
+    float pos_station[3];
+    Xvt sat_xvt;
+    SatID prn;
+} rtk_obs_t;
+
+void rtk_solver(vector<rtk_obs_t> &rtk_obs, Vector3d station_pos);
+
+#endif
